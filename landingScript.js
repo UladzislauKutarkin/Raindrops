4fd0dc6435bdfcfa5b010dd2c6cc8c9a7362546d
UladzislauKutarkin <vkutarkin@gmail.com>
2020-12-09 12:55:16 +0300

    innit: started task

---
+++ landingScript.js
@@ -0,0 +1,6 @@
+let howToPlay = document.querySelector('.how__to__play'),
+    startGame = document.querySelector('.play__field');
+
+howToPlay.addEventListener('click', (e)=> {
+    document.location='./tutorial.html'
+})
